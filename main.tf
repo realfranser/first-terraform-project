2122d28183dfff86575189afa1575bb7b068bd01
realfranser <serranoarresejavier@gmail.com>
2022-09-04 23:20:56 +0200

    reorganiced stuff

--- main.tf
+++ main.tf
@@ -1,20 +1,30 @@
 # Configure the AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.29.0"
+    }
+  }
+}
+
 provider "aws" {
   region     = var.region
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
-# Create VPC
+# 1.- Create VPC
 resource "aws_vpc" "main-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
     "Name" = "main vpc"
   }
 }
 
-# Create Internet Gateway
+# 2.- Create Internet Gateway
 resource "aws_internet_gateway" "main-internet-gateway" {
   vpc_id = aws_vpc.main-vpc.id
 
@@ -23,12 +33,12 @@ resource "aws_internet_gateway" "main-internet-gateway" {
   }
 }
 
-# Create route table
+# 3.- Create route table
 resource "aws_route_table" "main-route-table" {
   vpc_id = aws_vpc.main-vpc.id
 
   route {
-    cidr_block = var.default_route
+    cidr_block = "10.0.1.0/24"
     gateway_id = aws_internet_gateway.main-internet-gateway.id
   }
 
@@ -42,7 +52,7 @@ resource "aws_route_table" "main-route-table" {
   }
 }
 
-# Create a subnet
+# 4.- Create a subnet
 resource "aws_subnet" "main-subnet" {
   vpc_id            = aws_vpc.main-vpc.id
   cidr_block        = "10.0.1.0/24"
@@ -53,13 +63,13 @@ resource "aws_subnet" "main-subnet" {
   }
 }
 
-# Associate subnet to route table
+# 5.- Associate subnet to route table
 resource "aws_route_table_association" "main-route-table-association" {
   subnet_id      = aws_subnet.main-subnet.id
   route_table_id = aws_route_table.main-route-table.id
 }
 
-# Create security group to allow 22, 80, 443 traffic
+# 6.- Create security group to allow 22, 80, 443 traffic
 resource "aws_security_group" "main-security-group" {
   name        = "allow_web_traffic"
   description = "Allow inbound web traffic"
@@ -101,7 +111,7 @@ resource "aws_security_group" "main-security-group" {
   }
 }
 
-# Create a network interface with an ip in the main subnet
+# 7.- Create a network interface with an ip in the main subnet
 resource "aws_network_interface" "main-network-interface" {
   subnet_id       = aws_subnet.main-subnet.id
   private_ips     = ["10.0.1.50"]
@@ -112,7 +122,7 @@ resource "aws_network_interface" "main-network-interface" {
   }
 }
 
-# Assign an elastic IP to the main network interface
+# 8.- Assign an elastic IP to the main network interface
 resource "aws_eip" "main-elastic-ip" {
   vpc                       = true
   network_interface         = aws_network_interface.main-network-interface.id
@@ -124,13 +134,14 @@ resource "aws_eip" "main-elastic-ip" {
   }
 }
 
-# Create EC2 instance
+# 9.- Create EC2 instance
 resource "aws_instance" "main-server" {
   # ami								= var.amazon_linux_ami
   ami               = var.ubuntu_ami
   instance_type     = "t2.micro" # Free tier EC2 instance
   availability_zone = var.availability_zone
   key_name          = "main-key"
+  depends_on        = [aws_internet_gateway.main-internet-gateway]
 
   network_interface {
     device_index         = 0
